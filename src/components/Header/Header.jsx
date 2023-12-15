15acf060cd0f30a8fa7d950851a15e150e4ceaa7
Kostyantyn Tarusin <122443715+kostarusin@users.noreply.github.com>
2023-12-15 20:07:22 +0100

    build

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import style from "./Header.module.css";
-import Navigation from "../Navigation/Navigation";
+import Navigation from "../../components/Navigation/Navigation";
 
 const Header = () => {
   return <div className={style.headerWrapper}>{<Navigation />}</div>;
