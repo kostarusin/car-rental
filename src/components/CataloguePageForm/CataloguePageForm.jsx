74d138a875a5a633adfe668e8927282ed6240e67
Kostyantyn Tarusin <122443715+kostarusin@users.noreply.github.com>
2024-01-18 00:10:05 +0100

    form styling

--- src/components/CataloguePageForm/CataloguePageForm.jsx
+++ src/components/CataloguePageForm/CataloguePageForm.jsx
@@ -9,19 +9,50 @@ const CataloguePageForm = () => {
           <label className={style.label} for="brand">
             Car brand
           </label>
-          <input type="text" id="brand" className={style.brandInp} />
+          <input
+            type="text"
+            placeholder="Enter the text"
+            id="brand"
+            list="browsers"
+            className={style.brandInp}
+          />
+          {/* <datalist id="browsers">
+            <option>Edge</option>
+            <option>Firefox</option>
+            <option>Chrome</option>
+            <option>Opera</option>
+            <option>Safari</option>
+          </datalist> */}
         </div>
         <div className={style.inputWrapper}>
           <label className={style.label} for="price">
             Price / 1 hour
           </label>
-          <input type="text" id="price" className={style.priceInp} />
+          <input
+            type="text"
+            placeholder="To $"
+            id="price"
+            className={style.priceInp}
+          />
         </div>
         <div className={style.inputWrapper}>
           <label className={style.label} for="mileage">
             Car mileage / km
           </label>
-          <input type="text" id="mileage" className={style.mileageInp} />
+          <div>
+            <input
+              type="text"
+              placeholder="From"
+              id="mileage"
+              className={style.mileageInpFrom}
+            />
+            <input
+              type="text"
+              placeholder="To"
+              id="mileage"
+              className={style.mileageInpTo}
+            />
+          </div>
         </div>
         <button type="submit" className={style.button}>
           Search
