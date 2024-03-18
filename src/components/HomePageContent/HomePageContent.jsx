56f652458595e0b625fc7d71dbf26d92ecfdc9d4
Kostyantyn Tarusin <122443715+kostarusin@users.noreply.github.com>
2024-03-18 11:05:22 +0100

    HomePage

--- src/components/HomePageContent/HomePageContent.jsx
+++ src/components/HomePageContent/HomePageContent.jsx
@@ -7,7 +7,7 @@ const HomePageContent = () => {
     <section className={style.HomePageWrapper}>
       <h1 className={style.HomePageTitle}>RentUa</h1>
       <h2 className={style.HomePageTitle}>
-        The easiest way to rent a car in Ukraine!
+        The easiest way to rent a unic car in Ukraine!
       </h2>
       <Link className={style.HomePageLink} to="/catalogue">
         Learn more
